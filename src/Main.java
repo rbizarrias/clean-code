74750f8b5ee8bdd2eaddf5370d56c3177df8885a
rbizarrias <rivaldo.bizarrias@hotmail.com>
2026-07-31 08:46:34 -0300

    initial code

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 import com.objectmentor.utilities.args.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Args arg = new Args("l,p#,d*", args);
-            boolean logging = arg.getBoolean('l');
-            int port = arg.getInt('p');
-            String directory = arg.getString('d');
-            executeApplication(logging, port, directory);
-        } catch (ArgsException e) {
-            System.out.printf("Argument error: %s\n", e.errorMessage());
-        }
+        Args arg = new Args("l,p#,d*", args);
+        boolean logging = arg.getBoolean('l');
+        executeApplication(logging);
     }
 
-    private static void executeApplication(boolean logging, int port, String directory) {
-        System.out.printf("logging is %s, port:%d, directory:%s\n",logging, port, directory);
+    private static void executeApplication(boolean logging) {
+        System.out.printf("logging is %s",logging);
     }
 }
